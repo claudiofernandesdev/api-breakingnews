fc15091fa669798153bcd1a10bc93badbd2bb38c
claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-01-16 23:04:46 -0300

    route post created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const userRoute = require('./src/routes/user.route');
 const app = express();
+const userRoute = require('./src/routes/user.route');
+
 const port = 3000;
 
-app.use('/home', userRoute);
-  
-  app.listen(port);
+app.use(express.json());
+app.use('/user', userRoute);
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,20 @@
-const home = (req, res) => {
-    res.send('Hello World');
+const create = (req, res) => {
+    const {name, userName, email, password, avatar, background} = req.body;
+        if (!name || !userName || !email || !password || !avatar || !background) {
+            res.status(400).send({message: "Submit all fields are required"});
+        };
+    res.status(201).send({
+        message: "User created successfully",
+        user: {
+            name,
+            userName,
+            email,
+            avatar,
+            background
+        }
+    });
+
+    console.log(user);
 };
 
-module.exports = {home};
+module.exports = { create };
