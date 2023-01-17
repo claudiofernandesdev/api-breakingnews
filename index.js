0b056ed4c93611a4ae3c94976122374f903a1c95
claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-01-16 21:35:22 -0300

    added the software architeture

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const userRoute = require('./src/routes/user.route');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
+app.use('/home', userRoute);
   
   app.listen(port);
